523c6e73da85c41aeade3cc04cdef36107a8576f
kaing615 <kainguyen615@gmail.com>
2026-03-23 19:00:13 +0700

    feat: add database models for cinema management

---
+++ Models/ChiTietTheLoai.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Management_App.Models
+{
+    public class ChiTietTheLoai
+    {
+        public int MaPhim { get; set; }
+        public int MaTheLoai { get; set; }
+    }
+}
--- Models/Phim.cs
+++ Models/Phim.cs
@@ -8,6 +8,12 @@ namespace Cinema_Management_App.Models
 {
     public class Phim
     {
-
+        public int MaPhim { get; set; }
+        public string TenPhim { get; set; }
+        public int ThoiLuong { get; set; }
+        public int MaNhanPhim { get; set; }
+        public string TenDaoDien { get; set; }
+        public string TenDienVienChinh { get; set; }
+        public DateTime NgayKhoiChieu { get; set; }
     }
 }
