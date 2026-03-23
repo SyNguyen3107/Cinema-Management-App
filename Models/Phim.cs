5342493dd909cb48500d56197b9ad55e408570a2
Tam Nguyen Dinh <kainguyen615@gmail.com>
2026-03-23 23:03:35 +0700

    feat: add cinema management models (#3)
    * feat: remove ChiTietTheLoai model and add genre id array to Phim
    
    - remove ChiTietTheLoai class from Models
    - add MaTheLoais property to Phim
    - simplify model structure for repository-based mapping
    
    * feat: add database models for cinema management
    
    * feat: remove ChiTietTheLoai model and add genre id array to Phim
    
    - remove ChiTietTheLoai class from Models
    - add MaTheLoais property to Phim
    - simplify model structure for repository-based mapping
    
    * fix: remove conflict
    
    * Rename property MaTheLoai to DanhSachMaTheLoai
    
    * Change DanhSachMaTheLoai from array to List

---
+++ Models/Phim.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Management_App.Models
+{
+    public class Phim
+    {
+        public int MaPhim { get; set; }
+        public string TenPhim { get; set; }
+        public int ThoiLuong { get; set; }
+        public int MaNhanPhim { get; set; }
+        public string TenDaoDien { get; set; }
+        public string TenDienVienChinh { get; set; }
+        public DateTime NgayKhoiChieu { get; set; }
+
+        public List<int> DanhSachMaTheLoai { get; set; } = new List<int>();
+    }
+}
---
+++ Models/ThamSo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Management_App.Models
+{
+    public class ThamSo
+    {
+        public int MaThamSo { get; set; }
+        public int SoLuongTheLoaiToiDa { get; set; }
+    }
+}
