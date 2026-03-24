7d52b6228d22c9eb8860b2439f788be325f69f2d
Lythean763 <lythean763@gmail.com>
2026-03-24 10:27:55 +0700

    Hoan thanh giao dien

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Cinema_Management_App.Models;
+using Cinema_Management_App.Repositories;
 
 namespace Cinema_Management_App
 {
@@ -11,4 +13,26 @@ namespace Cinema_Management_App
     {
     }
 
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // 1. Đăng ký Services (Dùng AddSingleton: Tạo 1 lần, dùng chung mãi mãi)
+            services.AddSingleton<MySQLService>();
+
+            // 2. Đăng ký Repositories (Dùng AddTransient: Cần lúc nào tạo lúc đó)
+            services.AddTransient<PhimRepository>();
+
+            // 3. Đăng ký ViewModels
+            //services.AddTransient<ThemPhimViewModel>();
+            services.AddTransient<CapNhatPhimViewmodel>();
+            services.AddTransient<TiepNhanPhimViewmodel>();
+            services.AddTransient<XoaPhimViewmodel>();
+            //4. Đăng ký Views
+            services.AddTransient<TiepNhanPhimView>();
+            services.AddTransient<CapNhatPhimView>();
+            services.AddTransient<XoaPhimView>();
+            return services.BuildServiceProvider();
+        }
+    }
 }
