13711f14cc1e25f6b77543a4c4f1eee1639597d1
TheAn_X <lythean763@gmail.com>
2026-03-24 20:51:44 +0700

    Hoan thanh giao dien

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,37 +1,46 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
 using System.Windows;
-using Cinema_Management_App.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Cinema_Management_App.Services;
 using Cinema_Management_App.Repositories;
+using Cinema_Management_App.Viewmodels;
+using Cinema_Management_App.Views;
+using Application = System.Windows.Application;
 
 namespace Cinema_Management_App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        // Tạo bộ cung cấp dịch vụ toàn cục
+        public IServiceProvider Services { get; }
+
+        public new static App Current => (App)Application.Current;
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
 
         private static IServiceProvider ConfigureServices()
         {
             var services = new ServiceCollection();
 
-            // 1. Đăng ký Services (Dùng AddSingleton: Tạo 1 lần, dùng chung mãi mãi)
+            // 1. Đăng ký Services
             services.AddSingleton<MySQLService>();
 
-            // 2. Đăng ký Repositories (Dùng AddTransient: Cần lúc nào tạo lúc đó)
+            // 2. Đăng ký Repositories
             services.AddTransient<PhimRepository>();
 
             // 3. Đăng ký ViewModels
-            //services.AddTransient<ThemPhimViewModel>();
             services.AddTransient<CapNhatPhimViewmodel>();
             services.AddTransient<TiepNhanPhimViewmodel>();
             services.AddTransient<XoaPhimViewmodel>();
-            //4. Đăng ký Views
+
+            // 4. Đăng ký Views
             services.AddTransient<TiepNhanPhimView>();
             services.AddTransient<CapNhatPhimView>();
             services.AddTransient<XoaPhimView>();
+
             return services.BuildServiceProvider();
         }
     }
